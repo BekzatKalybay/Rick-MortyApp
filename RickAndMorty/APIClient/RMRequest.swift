a4520a0c33ffe0660628ddaba2279a3169e7bce0
Bekzat Kalybayev <bekzat92@gmail.com>
2023-01-27 10:15:31 +0600

    Create API files and add Documentation

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Bekzat Kalybayev on 26.01.2023.
+//
+
+import Foundation
+
+/// Object that represends single API Call
+final class RMRequest {
+    
+}
